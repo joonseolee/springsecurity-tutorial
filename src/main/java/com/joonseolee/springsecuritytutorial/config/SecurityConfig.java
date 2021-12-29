66135a647c0718ebf3c39c3648a64f888295fc01
joonseolee <niceweather94@gmail.com>
2021-12-29 23:04:44 +0900

    fix(): 누락된 파일 추가

--- src/main/java/com/joonseolee/springsecuritytutorial/config/SecurityConfig.java
+++ src/main/java/com/joonseolee/springsecuritytutorial/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.joonseolee.springsecuritytutorial.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,9 +10,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Slf4j
 @Configuration
 @EnableWebSecurity
+@Order(0)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/admin/**")
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
     }
 }
