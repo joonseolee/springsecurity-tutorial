11004061f9470a87d84f7fe2ab1826e114a7b21a
joonseolee <niceweather94@gmail.com>
2022-01-03 18:02:44 +0900

    feat(archiecture): 인증개념이해

--- src/main/java/com/joonseolee/springsecuritytutorial/config/SecurityConfig.java
+++ src/main/java/com/joonseolee/springsecuritytutorial/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Slf4j
 @Configuration
@@ -13,5 +14,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated();
+
+        http
+                .formLogin();
+
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
 }
--- src/main/java/com/joonseolee/springsecuritytutorial/controller/SecurityController.java
+++ src/main/java/com/joonseolee/springsecuritytutorial/controller/SecurityController.java
@@ -1,14 +1,34 @@
 package com.joonseolee.springsecuritytutorial.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 public class SecurityController {
 
     @GetMapping
-    public String index() {
+    public String index(HttpSession session) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        // 인증된 이후에는 세션에도 저장이 됨 (확인가능)
+        SecurityContext context = (SecurityContext) session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
         return "home";
+    }
+
+    @GetMapping("/thread")
+    public String thread() {
+        new Thread(() -> {
+            // 기본전략때문에 다른 쓰레드에서는 확인할수없음.
+                    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                }
+        ).start();
+
+        return "thread";
     }
 
     @GetMapping("loginPage")
