073d7e80ee5ab14b2cd8eb10ec25b158d30f8550
joonseolee <niceweather94@gmail.com>
2022-01-24 20:17:00 +0900

    feat(): 인증필터 추가

--- src/main/java/com/joonseolee/springsecuritytutorial/security/configs/SecurityConfig.java
+++ src/main/java/com/joonseolee/springsecuritytutorial/security/configs/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.joonseolee.springsecuritytutorial.security.configs;
 
+import com.joonseolee.springsecuritytutorial.security.filter.AjaxLoginProcessingFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -13,6 +15,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -43,6 +46,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling()
                 .accessDeniedHandler(accessDeniedHandler)
                 .and()
+                // UsernamePasswordAuthenticationFilter 필터앞에 필터를 추가한다.
+                .addFilterBefore(ajaxLoginProcessingFilter(), UsernamePasswordAuthenticationFilter.class)
+                // 토큰검사를 위해 일시 비활성화
+                .csrf().disable()
                 .formLogin()
                 .loginPage("/login")
                 .loginProcessingUrl("/login_proc")
@@ -58,5 +65,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         // 정적파일들은 보안필터를 아예 통과하지않고 패스
         web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
+    public AjaxLoginProcessingFilter ajaxLoginProcessingFilter() throws Exception {
+        var ajaxLoginProcessingFilter = new AjaxLoginProcessingFilter();
+        // AuthenticationManager 가 없을경우 filter 가 제대로 추가가 되지않음.
+        ajaxLoginProcessingFilter.setAuthenticationManager(authenticationManagerBean());
+        return ajaxLoginProcessingFilter;
     }
 }
---
+++ src/main/java/com/joonseolee/springsecuritytutorial/security/filter/AjaxLoginProcessingFilter.java
@@ -0,0 +1,45 @@
+package com.joonseolee.springsecuritytutorial.security.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.joonseolee.springsecuritytutorial.domain.AccountDto;
+import com.joonseolee.springsecuritytutorial.security.token.AjaxAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AjaxLoginProcessingFilter extends AbstractAuthenticationProcessingFilter {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    public AjaxLoginProcessingFilter() {
+        super(new AntPathRequestMatcher("/api/login"));
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+        if (!isAjax(request)) {
+            throw new IllegalStateException("Authentication is not supported.");
+        }
+
+        var accountDto = objectMapper.readValue(request.getReader(), AccountDto.class);
+        if (!StringUtils.hasText(accountDto.getUsername()) || !StringUtils.hasText(accountDto.getPassword())) {
+            throw new IllegalArgumentException("Username or Password is empty.");
+        }
+
+        var ajaxAuthenticationToken =
+                new AjaxAuthenticationToken(accountDto.getUsername(), accountDto.getPassword());
+
+        return getAuthenticationManager().authenticate(ajaxAuthenticationToken);
+    }
+
+    private boolean isAjax(HttpServletRequest request) {
+        return "XMLHttpRequest".equals(request.getHeader("X-Requested-With"));
+    }
+}
